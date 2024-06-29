cca120b3806eef155926b2b70778856a7722fae5
Barthélemy <31370477+BarthPaleologue@users.noreply.github.com>
2024-06-29 13:05:18 +0200

    replace contact icons by emojis

--- src/app/contactContainer.tsx
+++ src/app/contactContainer.tsx
@@ -7,10 +7,10 @@ export const ContactContainer = ({ }) => {
         <section className="contact">
             <h2><span className="icon contact-icon inverted"></span>{IS.contact}</h2>
             <div>
-                <p><span className="icon location-icon"></span>{you.location}</p>
-                <p><span className="icon phone-icon"></span><a href={`tel:${compactPhoneNumber}`}>{you.phoneNumber}</a></p>
-                <p><span className="icon portfolio-icon"></span><a href={you.onlinePortfolio} target="_blank">{IS.onlinePortfolio}</a></p>
-                <p><span className="icon email-icon"></span><a href={`mailto:${you.email}`}>{you.email}</a></p>
+                <p>📍&nbsp;&nbsp;{you.location}</p>
+                <p>📞&nbsp;&nbsp;<a href={`tel:${compactPhoneNumber}`}>{you.phoneNumber}</a></p>
+                <p>🔗&nbsp;&nbsp;<a href={you.onlinePortfolio} target="_blank">{IS.onlinePortfolio}</a></p>
+                <p>📭&nbsp;&nbsp;<a href={`mailto:${you.email}`}>{you.email}</a></p>
             </div>
         </section>
     )
