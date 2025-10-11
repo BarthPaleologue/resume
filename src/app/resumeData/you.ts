3488908ff59c9252d7aa436f7aba73106573ca96
inesvar <90340779+inesvar@users.noreply.github.com>
2025-10-11 16:50:49 +0200

    header.tsx: Use personal information from you.ts
    - reuse existing `you.name` from you.ts
    - move hardcoded personal info to a new `lookingFor` field in you.tx.

--- src/app/resumeData/you.ts
+++ src/app/resumeData/you.ts
@@ -1,7 +1,22 @@
-export const you = {
+import { S } from "../internationalization";
+
+export type You = {
+  name: string;
+  phoneNumber: string;
+  email: string;
+  onlinePortfolio?: string;
+  location: string;
+  lookingFor: string;
+};
+
+export const you: You = {
   name: "Barthélemy Paléologue",
   phoneNumber: "+33 6 37 29 67 37",
   email: "barth@paleologue.fr",
   onlinePortfolio: "https://barth.paleologue.fr",
   location: "Palaiseau, France",
+  lookingFor: S({
+    fr: "A la recherche d'un poste d'ingénieur en informatique graphique",
+    en: "Looking for a position as a computer graphics engineer",
+  }),
 };
